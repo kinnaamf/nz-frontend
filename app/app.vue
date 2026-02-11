8a2c6aa615b4d017ae3e324b0625b18cf1dacce5
kinnaamf <dimoje.1337@gmail.com>
2026-02-11 22:30:55 +0200

    feat: add nuxt fonts, start developing homepage

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,13 @@
 <template>
-
+  <div>
+    <main>
+      <NuxtPage/>
+    </main>
+  </div>
 </template>
+
+<style>
+* {
+  font-family: 'Geist', sans-serif;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside>
+    <div>
+      <IconBag class="h-3 w-3"/>
+      <span>Магазин</span>
+    </div>
+  </aside>
+</template>
+
+<script setup lang="ts">
+
+import IconBag from "~/ui/IconBag.vue";
+</script>
+
+
+<style scoped>
+
+</style>
