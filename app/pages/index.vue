c27a9633a72d8315985c409bd84e222face01999
kinnaamf <dimoje.1337@gmail.com>
2026-02-11 22:59:06 +0200

    feat: add some dynamic

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,37 +5,37 @@
         <div>
           <div class="flex items-center gap-2">
             <IconBag class="h-3 w-3"/>
-            <span>Магазин</span>
+            <NuxtLink to="/">Магазин</NuxtLink>
           </div>
           <div class="flex items-center gap-2">
             <IconUser class="h-3 w-3"/>
-            <span>Личный кабинет</span>
+            <NuxtLink to="/">Личный кабинет</NuxtLink>
           </div>
           <div class="mt-4 ml-5">
-            <ul>
-              <li>faq</li>
-              <li>Контакты</li>
-              <li>Клиентский сервис</li>
+            <ul v-for="el in topList">
+              <li>
+                <NuxtLink :to="el.to">{{ el.text }}</NuxtLink>
+              </li>
             </ul>
           </div>
         </div>
         <div>
-          <ul>
-            <li>Договор оферты</li>
-            <li>Политика конфиденциальности</li>
-            <li>Условия доставки</li>
+          <ul v-for="el in bottomList">
+            <li>
+              <NuxtLink :to="el.to">{{ el.text }}</NuxtLink>
+            </li>
           </ul>
         </div>
       </div>
     </aside>
     <main class="flex-1 flex flex-col gap-5 min-w-0">
-        <header>
-          <IconLogo/>
-        </header>
+      <header>
+        <IconLogo/>
+      </header>
 
-        <div class="flex-1 min-h-0">
-          <img src="/background.png" alt="" class="w-full h-full object-cover"/>
-        </div>
+      <div class="flex-1 min-h-0">
+        <img src="/background.png" alt="" class="w-full h-full object-cover"/>
+      </div>
     </main>
   </div>
 </template>
@@ -45,9 +45,16 @@
 import IconBag from "~/ui/IconBag.vue";
 import IconUser from "~/ui/IconUser.vue";
 import IconLogo from "~/ui/IconLogo.vue";
+
+const topList = [
+  { to: '/', text: 'faq' },
+  { to: '/', text: 'Контакты' },
+  { to: '/', text: 'Клиентский сервис' },
+]
+
+const bottomList = [
+  { to: '/', text: 'Договор оферты' },
+  { to: '/', text: 'Политика конфиденциальности' },
+  { to: '/', text: 'Условия доставки' },
+]
 </script>
-
-
-<style scoped>
-
-</style>
