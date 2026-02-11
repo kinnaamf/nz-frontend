7154f70d1cc77c8aae8aabd19186487abce913e5
kinnaamf <dimoje.1337@gmail.com>
2026-02-11 23:05:16 +0200

    feat: add sidebar component,
    fix: change v-for location

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,33 +1,7 @@
 <template>
   <div class="uppercase h-full p-5 w-full flex gap-20">
-    <aside class="font-normal text-sm h-full w-[23.75rem]">
-      <div class="flex flex-col justify-between h-full">
-        <div>
-          <div class="flex items-center gap-2">
-            <IconBag class="h-3 w-3"/>
-            <NuxtLink to="/">Магазин</NuxtLink>
-          </div>
-          <div class="flex items-center gap-2">
-            <IconUser class="h-3 w-3"/>
-            <NuxtLink to="/">Личный кабинет</NuxtLink>
-          </div>
-          <div class="mt-4 ml-5">
-            <ul v-for="el in topList">
-              <li>
-                <NuxtLink :to="el.to">{{ el.text }}</NuxtLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <ul v-for="el in bottomList">
-            <li>
-              <NuxtLink :to="el.to">{{ el.text }}</NuxtLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </aside>
+    <AppSidebar/>
+
     <main class="flex-1 flex flex-col gap-5 min-w-0">
       <header>
         <IconLogo/>
@@ -42,19 +16,7 @@
 
 <script setup lang="ts">
 
-import IconBag from "~/ui/IconBag.vue";
-import IconUser from "~/ui/IconUser.vue";
 import IconLogo from "~/ui/IconLogo.vue";
+import AppSidebar from "~/components/AppSidebar.vue";
 
-const topList = [
-  { to: '/', text: 'faq' },
-  { to: '/', text: 'Контакты' },
-  { to: '/', text: 'Клиентский сервис' },
-]
-
-const bottomList = [
-  { to: '/', text: 'Договор оферты' },
-  { to: '/', text: 'Политика конфиденциальности' },
-  { to: '/', text: 'Условия доставки' },
-]
 </script>
